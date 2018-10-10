7789b1d364d40a59b1a85357912dadac765c6acf
Craig Crosby <creecros@gmail.com>
2018-10-10 17:18:59 -0400

    Update menu.php

--- Template/board/menu.php
+++ Template/board/menu.php
@@ -1,4 +1,11 @@
+<?php if ($this->ProjectUsesBacklogBoardModel->issetBacklogBoard($project['id'])): ?>
         <li>
         <i class="fa fa-plus-square-o fa-fw"></i>
-        <?= $this->url->link(t('Change to Backlog Board'), 'BacklogBoardController', 'task', ['plugin' => 'backlog', 'projectid' => $project['id']]) ?>
+        <?= $this->url->link(t('Change to Backlog Board'), 'BacklogBoardController', 'set', ['plugin' => 'backlog']) ?>
         </li>
+<?php else: ?>
+        <li>
+        <i class="fa fa-plus-square-o fa-fw"></i>
+        <?= $this->url->link(t('Change to Normal Board'), 'BacklogBoardController', 'unset', ['plugin' => 'backlog']) ?>
+        </li>
+<?php endif ?>
