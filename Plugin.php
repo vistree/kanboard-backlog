903cd78486ca94e00694f92be99e32286dfe5a79
Craig Crosby <creecros@gmail.com>
2018-10-10 17:21:54 -0400

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -8,28 +8,43 @@ class Plugin extends Base
 {
     public function initialize()
     {
-      $this->template->setTemplateOverride('board/table_container','backlog:board/table_container');     
+        $this->template->setTemplateOverride('board/table_container','backlog:board/table_container'); 
+        $this->template->hook->attach('template:board:column:dropdown', 'backlog:board/menu');
     }
+    
+    public function getClasses() {
+        return array(
+            'Plugin\Backlog\Model' => array(
+                'ProjectUsesBacklogBoardModel',
+            )
+        );
+    }
+    
     public function getPluginName()
     {
         return 'Backlog';
     }
+    
     public function getPluginDescription()
     {
         return t('Plugin to add a backlog column with full height to project board');
     }
+    
     public function getPluginAuthor()
     {
         return 'vistree';
     }
+    
     public function getPluginVersion()
     {
-        return '0.0.1';
+        return '0.0.2';
     }
+    
     public function getPluginHomepage()
     {
         return 'https://github.com/vistree/kanboard-backlog';
     }
+    
     public function getCompatibleVersion()
     {
         return '>=1.2.4';
