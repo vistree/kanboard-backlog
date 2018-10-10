266642d9ab453f574cd54988b49dd3128653030d
Craig Crosby <creecros@gmail.com>
2018-10-10 18:08:40 -0400

    Update menu.php

--- Template/board/menu.php
+++ Template/board/menu.php
@@ -1,4 +1,4 @@
-<?php if ($this->projectMetadataModel->exists($project['id'], 'uses_backlogboard')): ?>
+<?php if ($this->task->projectUsesBacklogBoardModel->isset($project['id'])): ?>
         <li>
         <i class="fa fa-plus-square-o fa-fw"></i>
         <?= $this->url->link(t('Change to Backlog Board'), 'BacklogBoardController', 'set', ['plugin' => 'backlog']) ?>
