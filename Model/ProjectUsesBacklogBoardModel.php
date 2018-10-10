480dda3389dd5593bf3de46acaccd43bd7a008bc
Craig Crosby <creecros@gmail.com>
2018-10-10 18:02:11 -0400

    Update ProjectUsesBacklogBoardModel.php

--- Model/ProjectUsesBacklogBoardModel.php
+++ Model/ProjectUsesBacklogBoardModel.php
@@ -17,11 +17,4 @@ class ProjectUsesBacklogBoardModel extends Base {
         $this->projectMetadataModel->remove($project_id, 'uses_backlogboard');
     }
     
-    public function issetBacklogBoard($project_id) {
-    
-        $set = $this->projectMetadataModel->get($project_id, 'uses_backlogboard');
-        if (!$set)
-          return(null);
-        return true;
-    }
 }
