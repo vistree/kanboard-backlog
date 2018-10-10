172a4400ee903236840a971732f3e5cff81707ed
Craig Crosby <creecros@gmail.com>
2018-10-09 23:41:43 -0400

    Update BacklogBoardController.php

--- Controller/BacklogBoardController.php
+++ Controller/BacklogBoardController.php
@@ -3,6 +3,9 @@
 namespace Kanboard\Plugin\Backlog\Controller;
 
 use Kanboard\Controller\BaseController;
+use Kanboard\Model\ProjectModel;
+use Kanboard\Model\SwimlaneModel;
+
 
 /**
  * Backlog Board
@@ -16,6 +19,7 @@ class BacklogBoardController extends BaseController {
         $project = $this->getProject();
 
         $this->projectUsesBacklogBoardModel->setBacklogBoard($project['id']);
+        $this->backlogSwimlane($project['id']);
 
         $this->flash->success(t('Backlog Board now activated.'));
 
@@ -26,10 +30,40 @@ class BacklogBoardController extends BaseController {
         $project = $this->getProject();
 
         $this->projectUsesBacklogBoardModel->unsetBacklogBoard($project['id']);
+        $this->removeBacklogSwimlane($project['id'], 0);
 
         $this->flash->success(t('Backlog Board now deactivated.'));
 
         $this->response->redirect($this->helper->url->to('BoardAjaxController', 'renderBoard', array('project_id' => $project['id'])), true);
+    }
+    
+    public function backlogSwimlane($projectId) {
+        if ($this->projectModel->exists($projectId)) {
+           
+          return $this->db->table(self::TABLE)->persist(array(
+            'project_id'  => $projectId,
+            'name'        => 'BacklogBoard_Swimlane',
+            'description' => 'Temporary Swimlane for Backlog Board',
+            'position'    => 0,
+            'is_active'   => 1,
+          ));
+            
+        }
+    }
+    
+    public function removeBacklogSwimlane($projecId, $swimlaneId) {
+        $this->db->startTransaction();
+        if ($this->db->table(TaskModel::TABLE)->eq('swimlane_id', $swimlaneId)->exists()) {
+            $this->db->cancelTransaction();
+            return false;
+        }
+        if (! $this->db->table(self::TABLE)->eq('id', $swimlaneId)->remove()) {
+            $this->db->cancelTransaction();
+            return false;
+        }
+        $this->swimlaneModel->updatePositions($projecId);
+        $this->db->closeTransaction();
+        return true;
     }
 
 }
