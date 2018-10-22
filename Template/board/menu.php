b55a71ffc88b54b26042b06c595ef21df7e4b2fe
vistree <fischerk@web.de>
2018-10-22 14:02:57 +0200

    limit access to backlog-menu-item to users with ProjectEditController-show permission

--- Template/board/menu.php
+++ Template/board/menu.php
@@ -1,11 +1,13 @@
-<?php if ($this->task->projectUsesBacklogBoardModel->backlogIsset($project['id'])): ?>
+<?php if ($this->user->hasProjectAccess('ProjectEditController', 'show', $project['id'])): ?>
+    <?php if ($this->task->projectUsesBacklogBoardModel->backlogIsset($project['id'])): ?>
         <li>
-        <i class="fa fa-plus-square-o fa-fw"></i>
-        <?= $this->url->link(t('Change to Normal Board'), 'BacklogBoardController', 'backlogUnset', ['plugin' => 'backlog', 'project_id' => $project['id']]) ?>
+            <i class="fa fa-plus-square-o fa-fw"></i>
+            <?= $this->url->link(t('Change to Normal Board'), 'BacklogBoardController', 'backlogUnset', ['plugin' => 'backlog', 'project_id' => $project['id']]) ?>
         </li>
-<?php else: ?>
+    <?php else: ?>
         <li>
-        <i class="fa fa-plus-square-o fa-fw"></i>
-        <?= $this->url->link(t('Change to Backlog Board'), 'BacklogBoardController', 'backlogSet', ['plugin' => 'backlog', 'project_id' => $project['id']]) ?>
+            <i class="fa fa-plus-square-o fa-fw"></i>
+            <?= $this->url->link(t('Change to Backlog Board'), 'BacklogBoardController', 'backlogSet', ['plugin' => 'backlog', 'project_id' => $project['id']]) ?>
         </li>
+    <?php endif ?>
 <?php endif ?>
