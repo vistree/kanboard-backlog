8eb49e58d35b64ab5b268547622d64b07dcad930
Craig Crosby <creecros@gmail.com>
2019-01-07 14:55:46 -0500

    Create view.php

---
+++ Template/board/view.php
@@ -0,0 +1,21 @@
+
+<section id="main">
+
+    <span class='header'><h1><?= $this->text->e($project['name']) ?>
+    
+    <?php if (! empty($project['description'])): ?>
+        <?= $this->app->tooltipMarkdown($project['description']) ?>
+    <?php endif ?>
+    
+    </span>
+    </h1></span>
+    
+
+    <?= $this->render('backlog:board/bb_table_container', array(
+        'project' => $project,
+        'swimlanes' => $swimlanes,
+        'board_private_refresh_interval' => $board_private_refresh_interval,
+        'board_highlight_period' => $board_highlight_period,
+    )) ?>
+
+</section>
